4c3ad91202e5a31b81cdfb9b454998628697daa9
iirquhs-np <82794055+iirquhs-np@users.noreply.github.com>
2022-02-13 21:18:05 +0800

    Minor modifications

--- js/account.js
+++ js/account.js
@@ -1,5 +1,5 @@
 const userLoggedIn = localStorage.getItem('userLoggedIn');
-const site = "https://comzone.shuqri.xyz/";
+const site = window.location.protocol + '//' + window.location.host + '/';
 // const site = "http://localhost:63342/IDAssignment2/"; // FOR DEBUGGING
 
 $(document).ready(function () {
