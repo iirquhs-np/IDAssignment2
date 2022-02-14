bf8c84f9a2ee158cfdbca07be31bc41a245f0633
Shuqri Shaifuddin <82794055+iirquhs-np@users.noreply.github.com>
2022-02-14 15:10:24 +0800

    Added back-to-top feature. Standardised all pages to have same navbar and footer.

---
+++ js/footer.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(".social-facebook").on("click", function () {
+        window.open("https://www.facebook.com/makeitnp/");
+    });
+    $(".social-google").on("click", function () {
+        window.open("https://goo.gl/maps/oqLvC57pu5BBTZGK7/");
+    });
+    $(".social-linkedin").on("click", function () {
+        window.open("https://www.linkedin.com/school/school-of-infocomm-technology-ngee-ann-polytechnic/");
+    });
+    $(".social-instagram").on("click", function () {
+        window.open("https://www.instagram.com/makeitnp/");
+    });
+    $(".social-twitter").on("click", function () {
+        window.open("https://twitter.com/makeitnp");
+    });
+});
